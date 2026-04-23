e2eada1b18aff25c8b8b80df107c15d68c070a0c
RashidWork97 <rashideen.work@gmail.com>
2026-04-23 14:35:49 +0800

    adjusting a few col n row

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ function App() {
   return (
     <>
       <div className="h-dvh min-h-[100vh] min-w-[1400px] p-8 box-border border-2">
-      <div className="h-full  bg-sky-100 rounded-lg shadow-lg p-4 font-corbel font-bold text-2xl text-left">
-        My Profile
-        <div className="grid grid-cols-10 grid-rows-10 gap-4">
-          <div className="col-span-1 row-span-10 border rounded-sm"></div>
-          <div className="col-span-5 col-start-2 row-span-20 border rounded-sm"></div>
+      <div className="h-full bg-sky-400/10 rounded-lg shadow-lg p-4 font-corbel font-bold text-2xl text-left">
+         My Profile
+        <div className="bg-sky-100 backdrop-blur-xl grid grid-cols-10 gap-4">
+          <div className="col-span-1 col-start-2 row-span-10 border rounded-sm"></div>
+          <div className="col-span-3 col-start-5 row-span-40 border rounded-sm"></div>
         </div>
       </div>
     </div>
